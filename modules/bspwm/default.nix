a782ff81ea605cf514312441be485683d80b16d2
RazYang <xzzorz@gmail.com>
2022-12-13 22:44:32 +0800

    add missing file

---
+++ modules/bspwm/default.nix
@@ -0,0 +1,6 @@
+{
+    service.xserver.enable = true;
+    service.xserver.videoDrivers = [ "amdgpu" ];
+    service.xserver.displayManager.lightdm.enable = true;
+    service.xserver.windowManager.bspwm.enable = true;
+}
