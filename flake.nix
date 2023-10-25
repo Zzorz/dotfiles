3fd1c20b6b42e5c7493dc237c847a8826baf911d
yangtiangang <yangtiangang@hikvision.com.cn>
2023-10-25 11:54:25 +0800

    add lxc

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "RazYang's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
