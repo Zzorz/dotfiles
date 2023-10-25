5b2c1cb8dfd0075322aeb2dec93f0756e0d94d61
yangtiangang <yangtiangang@hikvision.com.cn>
2023-10-25 13:48:04 +0800

    change nixpkgs master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "RazYang's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git";
+    nixpkgs.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
