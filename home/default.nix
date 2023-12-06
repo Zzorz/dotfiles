1d47a562eb35c4d156cdb644694412d37ed0f3dd
RazYang <xzzorz@gamil.com>
2023-12-06 12:02:14 +0800

    fix zsh initExtra, remove ohmyzsh

--- home/default.nix
+++ home/default.nix
@@ -28,21 +28,20 @@
       enable = true;
       enableAutosuggestions = true;
       enableCompletion = true;
+      syntaxHighlighting.enable = true;
       defaultKeymap = "emacs";
       antidote = {
         enable = true;
 	plugins = [
 	  "romkatv/powerlevel10k"
 	  "z-shell/zsh-editing-workbench"
-	  "zdharma-continuum/fast-syntax-highlighting"
-	  "sorin-ionescu/prezto path:modules/completion kind:defer"
+	  "sorin-ionescu/prezto path:modules/completion"
 	  "hlissner/zsh-autopair kind:defer"
-	  "ohmyzsh/ohmyzsh path:lib"
-	  "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
 	];
       };
       completionInit = "";
       initExtraFirst = ''
+        typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
         # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
         # Initialization code that may require console input (password prompts, [y/n]
         # confirmations, etc.) must go above this block; everything else may go below.
@@ -51,8 +50,8 @@
         fi
       '';
       initExtra = ''
-        [[ ! -f "~/.cargo/env" ]] || source "~/.cargo/env"
-        [[ ! -f "~/.zsh_extra" ]] || source "~/.zsh_extra"
+        [[ ! -f ~/.cargo/env ]] || source ~/.cargo/env
+        [[ ! -f ~/.zsh_extra ]] || source ~/.zsh_extra
 	[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
       '';
     };
