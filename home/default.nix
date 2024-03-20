15aea00d2d4c793247cb5c065940da82ae83c519
RazYang <xzzorz@gamil.com>
2024-03-20 11:24:38 +0800

    flake update

--- home/default.nix
+++ home/default.nix
@@ -21,6 +21,29 @@
       userEmail = "xzzorz@gmail.com";
     };
     home-manager.enable = true;
+    zellij ={
+      enable = true;
+      enableBashIntegration = false;
+      settings = {
+        pane_frames = false;
+        theme = "gruvbox-dark";
+        themes = {
+          gruvbox-dark = {
+            fg = [213 196 161];
+            bg = [40 40 40];
+            black = [60 56 54];
+            red = [204 36 29];
+            green = [152 151 26];
+            yellow = [215 153 33];
+            blue = [69 133 136];
+            magenta = [177 98 134];
+            cyan = [104 157 106];
+            white = [251 241 199];
+            orange = [214 93 14];
+	  };
+	};
+      };
+    };
     tmux = {
       enable = true;
       plugins = with pkgs; [
@@ -33,7 +56,7 @@
     fzf = { enable = true; enableZshIntegration = true; };
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestion.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
       defaultKeymap = "emacs";
