f9991a2ca10d34ae6688b94a4b9b10257922a028
RazYang <xzzorz@gamil.com>
2024-05-21 19:06:44 +0800

    reconfig neovim

--- home-modules/neovim.nix
+++ home-modules/neovim.nix
@@ -6,6 +6,8 @@
     viAlias = true;
     vimdiffAlias = true;
     defaultEditor = true;
+    withPython3 = true;
+    withNodeJs = true;
 
     plugins = with pkgs.vimPlugins; [
       coc-clangd
@@ -26,66 +28,118 @@
       nvim-treesitter-refactor
       nvim-treesitter-textobjects
       nvim-treesitter-context
-      nvim-surround
-      telescope-nvim
       vim-airline
-      which-key-nvim
       gruvbox-nvim
-      indent-blankline-nvim
-      vim-sneak
       quick-scope
       copilot-vim
-      editorconfig-vim
       vim-visual-multi
+      editorconfig-vim
+
+      { 
+        plugin = nvim-surround;
+        type = "lua";
+        config = ''
+          require("nvim-surround").setup()
+        '';
+      }
+
+      {
+        plugin = indent-blankline-nvim;
+        type = "lua";
+        config = ''
+          require("ibl").setup()
+        '';
+      }
+
+      { 
+        plugin = vim-easymotion;
+        type = "vim";
+        config = ''
+          wk = require("which-key")
+          wk.register({
+          },{prefix = "<space>", noremap = true})
+        '';
+      }
+      { 
+        plugin = telescope-nvim;
+        type = "lua";
+        config = ''
+          local actions = require('telescope.actions')
+          require('telescope').setup {
+            defaults = {
+              mappings = {
+                i = {
+                  ["<C-j>"] = actions.move_selection_next,
+                  ["<C-k>"] = actions.move_selection_previous,
+                  ["<C-q>"] = actions.send_to_qflist,
+                },
+                n = {
+                  ["q"] = actions.close,
+                  ["<C-g>"] = actions.close,
+                },
+              },
+            },
+            pickers = {
+              buffers = {
+                sort_lastused = true,
+                theme = "dropdown",
+                previewer = false,
+                mappings = {
+                  i = {
+                    ["<C-d>"] = "delete_buffer",
+                  },
+                  n = {
+                    ["<C-d>"] = "delete_buffer",
+                  },
+                },
+              },
+            },
+
+          }
+        '';
+      }
+
+      {
+        plugin = which-key-nvim;
+        type = "lua";
+        config = ''
+          wk = require("which-key")
+          wk.setup {
+            window = {
+              border = "single"
+            }
+          }
+            
+          wk.register({
+            f = {
+              name = "file",
+              f = {"<cmd>Telescope find_files<cr>", "Find File"},
+              s = {"<cmd>Telescope live_grep<cr>", "Grep File"},
+            },
+            b = {
+              name = "buffer",
+              b = {"<cmd>Telescope buffers<cr>", "switch Buffer"},
+              s = {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "search in current Buffer"},
+            },
+            ["<space>"] = {
+              name = "easymotion",
+              f = {"<Plug>(easymotion-f)", "f{char} to move to {char}"},
+              s = {"<Plug>(easymotion-f2)", "s{char}{char} to move to {char}{char}"},
+              w = {"<Plug>(easymotion-w)", "Move to word"},
+              l = {"<Plug>(easymotion-bd-jk)", "Move to line"},
+            }
+          }, {prefix= "<space>",noremap = true})
+
+        '';
+      }
     ];
     coc = { enable = true; };
     extraConfig = ''
       set relativenumber
-      nnoremap ,f <cmd>Telescope find_files<cr>
-      nnoremap ,g <cmd>Telescope live_grep<cr>
-      nnoremap ,b <cmd>Telescope buffers<cr>
-      nnoremap ,c <cmd>Telescope commands<cr>
-      let g:qs_highlight_on_keys = ['f', 'F']
-
-      inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
-      nmap <silent> gd <Plug>(coc-definition)
-      nmap <silent> gy <Plug>(coc-type-definition)
-      nmap <silent> gi <Plug>(coc-implementation)
-      nmap <silent> gr <Plug>(coc-references)
-
-      nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
-      " Manage extensions
-      nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
-      " Show commands
-      nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
-      " Find symbol of current document
-      nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
-      " Search workspace symbols
-      nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
-      " Do default action for next item
-      nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
-      " Do default action for previous item
-      nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
-      " Resume latest coc list
-      nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
-
-      function! CheckBackspace() abort
-        let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-      endfunction
-      inoremap <silent><expr> <Tab>
-        \ coc#pum#visible() ? coc#pum#next(1) :
-        \ CheckBackspace() ? "\<Tab>" :
-        \ coc#refresh()
-
-      let g:indent_guides_enable_on_vim_startup = 1
-      let g:airline#extensions#tabline#enabled = 1
-      let g:sneak#label = 1
     '';
     extraLuaConfig = ''
       vim.o.background = "dark"
       vim.cmd([[colorscheme gruvbox]])
-      require("ibl").setup()
     '';
   };
 }
