75c670bceea6fda171a14ec2fe986a153b7e7b3c
RazYang <xzzorz@gamil.com>
2024-06-11 14:41:18 +0800

    add fzf-tab

--- home-modules/zsh.nix
+++ home-modules/zsh.nix
@@ -1,15 +1,11 @@
 { pkgs, ... }:
 {
-  programs.carapace.enable = true;
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    prezto = {
-      enable = true;
-      prompt.theme = "off";
-    };
+    defaultKeymap = "emacs";
     plugins = [
       {
         name = "powerlevel10k";
@@ -21,13 +17,29 @@
           sha256 = "sha256-ES5vJXHjAKw/VHjWs8Au/3R+/aotSbY7PWnWAMzCR8E=";
         };
       }
+      {
+        name = "fzf-tab";
+        file = "fzf-tab.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "Aloxaf";
+          repo = "fzf-tab";
+          rev = "v1.1.2";
+          sha256 = "sha256-Qv8zAiMtrr67CbLRrFjGaPzFZcOiMVEFLg1Z+N6VMhg=";
+        };
+      }
+      {
+        name = "zsh-editing-workbench";
+        src = pkgs.fetchFromGitHub {
+          owner = "z-shell";
+          repo = "zsh-editing-workbench";
+          rev = "v1.0.0";
+          sha256 = "sha256-087bNmB5gDUKoSriHIjXOVZiUG5+Dy9qv3D69E8GBhs=";
+        };
+      }
     ];
     completionInit = "";
     initExtraFirst = ''
       typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
@@ -37,7 +49,9 @@
       [[ ! -f ~/.cargo/env ]] || source ~/.cargo/env
       [[ ! -f ~/.zsh_extra ]] || source ~/.zsh_extra
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      #export LS_COLORS="$(vivid generate gruvbox-dark)"
+      zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+      CASE_SENSITIVE="true"
     '';
   };
 }
