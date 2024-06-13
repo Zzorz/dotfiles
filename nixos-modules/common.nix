8ecf2961d1365f84fb4ec015fdc43c800566f863
Yang Tiangang <yang@0xdeadbeef.local>
2024-06-14 02:05:29 +0800

    config nix in nixos

--- nixos-modules/common.nix
+++ nixos-modules/common.nix
@@ -1,5 +1,18 @@
 { self, inputs, pkgs, stateVersion, ... }:
 {
+  nix =
+    {
+      settings = rec {
+        auto-optimise-store = true;
+        substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+
+        trusted-substituters = substituters;
+      };
+      extraOptions = ''
+        extra-experimental-features = nix-command flakes
+        extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ]
+      '';
+    };
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "zh_CN.UTF-8";
